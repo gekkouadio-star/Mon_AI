ec28607857117d967c14ff62b7e6553dd8596105
KOUADIO <gerard@MacBook-Air-de-KOUADIO.local>
2026-03-01 13:00:35 +0100

    essay

--- main.py
+++ main.py
@@ -370,8 +370,7 @@ async def chat(request: ChatRequest):
 if __name__ == "__main__":
     import os
     import uvicorn
-    # On récupère le port dynamiquement (8000 par défaut en local)
+
     port = int(os.environ.get("PORT", 8000))
-    # On lance le serveur
-    uvicorn.run(app, host="127.0.0.1", port=port)
+    uvicorn.run(app, host="0.0.0.0", port=port)
  
